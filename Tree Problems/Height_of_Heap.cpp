9f42ce654d97860e27eb26f62bcb0d4b9f5c0761
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-11-19 00:00:17 +0530

    tree

---
+++ Tree Problems/Height_of_Heap.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class SOlve{
+    public:
+      int heapHeight(int n, int a[]){
+        return floor(log2(n));
+      }
+
+      int heapHeight_2nd_methd(int x,int b[]){
+        int m=1,k=-1;
+      }
+};
+int main(){
+    cout<<"Enter no of test cases : ";
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+
+    }
+}
