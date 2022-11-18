9925f20b0d3394ac897305cfec0704ec215ec636
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-11-19 00:21:37 +0530

    Tree

---
+++ Tree Problems/Size_of_Binary_Tree.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *left,*right;
+};
+
+Node * newNode(int data){
+    Node* temp= new Node;
+    temp->data=data;
+    temp->left=NULL;
+    temp->right=NULL;
+
+    return temp;
+}
+
+Node* treeInput(string s){
+
+if(s.length()==0 || s[0]=='N') return NULL; //base case;
+
+// creating vector of string from input
+vector<string>s1;
+// string after spiting by space
+istringstream iss(s);
+for(string s; iss>>s;) {s1.push_back(s);}
+
+//creating root of the tree
+Node* root=newNode(stoi(s1[0]));
+
+//push the root to queue
+queue<Node*>Q;
+Q.push(root);
+
+// staring from the second element 
+int i=1;
+while(!Q.empty() && i<s1.size()){
+    // get front of queue
+    Node* currNode=Q.front();
+    // remove front of queue
+    Q.pop();
+    //get the current node's value from the string
+    string s2=s1[i];
+    //if left child is not null
+    if(s2!="N"){
+        //create left child for the current node
+        currNode->left=newNode(stoi(s2));
+        //push currNode->left to queue;
+        Q.push(currNode->left);
+    }
+    //for right chile
+    i++;
+    if(i>=s1.size()) break;
+    s2=s1[i];
+    //if right child is not NULL
+    if(s2!="N"){
+        // creating right child for current node
+        currNode->right=newNode(stoi(s2));
+        //push to the queue
+        Q.push(currNode->right);
+    }
+    i++;
+}
+return root;
+}
+int getSize(Node* node)
+{
+  
+   if(node==NULL){ return 0;}
+   else{
+       return getSize(node->left)+getSize(node->right)+1;
+   }
+
+}
+int main(){
+   
+   
+        string s;
+        getline(cin,s);
+        Node* root =treeInput(s);
+        cout<<getSize(root)<<endl;
+    
+}
