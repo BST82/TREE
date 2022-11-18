9b2d377febf15f02d2dfc7f5dd12895c13363a9c
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-11-18 23:50:27 +0530

    Tree

--- Tree Problems/Count_Leaves_in_Binary_Tree.cpp
+++ Tree Problems/Count_Leaves_in_Binary_Tree.cpp
@@ -17,31 +17,46 @@ Node * newNode(int data){
 
 Node* treeInput(string s){
 
-if(s.length()==0 || s[0]=='N') return NULL;
+if(s.length()==0 || s[0]=='N') return NULL; //base case;
+
+// creating vector of string from input
 vector<string>s1;
+// string after spiting by space
 istringstream iss(s);
 for(string s; iss>>s;) {s1.push_back(s);}
 
+//creating root of the tree
 Node* root=newNode(stoi(s1[0]));
 
+//push the root to queue
 queue<Node*>Q;
 Q.push(root);
 
+// staring from the second element 
 int i=1;
 while(!Q.empty() && i<s1.size()){
+    // get front of queue
     Node* currNode=Q.front();
+    // remove front of queue
     Q.pop();
+    //get the current node's value from the string
     string s2=s1[i];
+    //if left child is not null
     if(s2!="N"){
+        //create left child for the current node
         currNode->left=newNode(stoi(s2));
+        //push currNode->left to queue;
         Q.push(currNode->left);
     }
     //for right chile
     i++;
     if(i>=s1.size()) break;
     s2=s1[i];
+    //if right child is not NULL
     if(s2!="N"){
+        // creating right child for current node
         currNode->right=newNode(stoi(s2));
+        //push to the queue
         Q.push(currNode->right);
     }
     i++;
