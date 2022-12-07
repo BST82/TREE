53991e6622d11c121e2fd3e94e79d171aa37d26c
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-12-07 10:36:40 +0530

    #

--- Tree Problems/257_Binary_Tree_Paths.cpp
+++ Tree Problems/257_Binary_Tree_Paths.cpp
@@ -13,3 +13,21 @@ Node* newNode(int val){
 
   return temp;
 }
+
+void path(vector<string>&ans, Node* root, string s){
+
+}
+
+vector<string>binaryTreePaths(Node* root){
+ vector<string>answewr;
+ if(!root) return answewr;
+
+ path(answewr,root,to_string(root->data));
+ return answewr;
+}
+int main(){
+     Node* root= newNode(1);
+     root->left=newNode(2);
+     root->right=newNode(3);
+     root->left->right=newNode(5);
+}
