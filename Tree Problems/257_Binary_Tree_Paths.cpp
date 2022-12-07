46ac20daa4748705051a449c67d19d5ffc2e6541
Bhagyashri Tamrakar <86471670+BST82@users.noreply.github.com>
2022-12-07 10:46:49 +0530

    257. Binary Tree Paths

--- Tree Problems/257_Binary_Tree_Paths.cpp
+++ Tree Problems/257_Binary_Tree_Paths.cpp
@@ -15,19 +15,37 @@ Node* newNode(int val){
 }
 
 void path(vector<string>&ans, Node* root, string s){
+     
+     if(!root->left && !root->right){
+        ans.push_back(s);
+        return ;
+     }
+     if(root->left){
+        path(ans,root->left,s + "->"+ to_string(root->left->data));
+     }
 
+     if(root->right){
+        path(ans,root->right,s +"->"+ to_string(root->right->data));
+     }
 }
 
 vector<string>binaryTreePaths(Node* root){
+
  vector<string>answewr;
  if(!root) return answewr;
-
  path(answewr,root,to_string(root->data));
  return answewr;
+
 }
 int main(){
      Node* root= newNode(1);
      root->left=newNode(2);
      root->right=newNode(3);
      root->left->right=newNode(5);
+
+     vector<string>s=binaryTreePaths(root);
+     for(auto &i:s){
+        cout<<i<<endl;
+     }
+     
 }
